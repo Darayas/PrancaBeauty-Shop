33c1f20a59f3af8899299bf637373762df9ef66e
Reza <Reza@DESKTOP-N211H1V>
2021-01-23 22:36:54 +0330

    چندزبانی - بخش اول

---
+++ PrancaBeauty.WebApp/Config/StartupEx.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.WebEncoders;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace PrancaBeauty.WebApp.Config
+{
+    public static class StartupEx
+    {
+        public static IServiceCollection WebEncoderConfig(this IServiceCollection services)
+        {
+            return services.Configure<WebEncoderOptions>(opt =>
+            {
+                opt.TextEncoderSettings = new TextEncoderSettings(UnicodeRanges.Arabic, UnicodeRanges.BasicLatin);
+            });
+        }
+
+        public static IMvcBuilder AddRazorPage(this IServiceCollection services)
+        {
+            return services.AddRazorPages(a =>
+            {
+                a.Conventions.AddPageRoute("/Home/Index", "");
+            });
+        }
+
+        public static IServiceCollection AddLocalization(this IServiceCollection services, string ResourcePath)
+        {
+            return services.AddLocalization(a => a.ResourcesPath = ResourcePath);
+        }
+
+        public static IApplicationBuilder UseLocalization(this IApplicationBuilder app, List<CultureInfo> SupportedLans, string DefCultureName = "fa-IR")
+        {
+            var Options = new RequestLocalizationOptions()
+            {
+                DefaultRequestCulture = new RequestCulture(DefCultureName),
+                SupportedCultures = SupportedLans,
+                SupportedUICultures = SupportedLans,
+                RequestCultureProviders = new List<IRequestCultureProvider>()
+                {
+                    new CookieRequestCultureProvider(),
+                    new QueryStringRequestCultureProvider()
+                }
+            };
+
+            return app.UseRequestLocalization(Options);
+        }
+    }
+}
--- PrancaBeauty.WebApp/Startup.cs
+++ PrancaBeauty.WebApp/Startup.cs
@@ -4,8 +4,10 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.WebEncoders;
+using PrancaBeauty.WebApp.Config;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text.Encodings.Web;
 using System.Text.Unicode;
@@ -17,12 +19,12 @@ namespace PrancaBeauty.WebApp
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<WebEncoderOptions>(opt =>
-            {
-                opt.TextEncoderSettings = new TextEncoderSettings(UnicodeRanges.Arabic, UnicodeRanges.BasicLatin);
-            });
+            services.AddLocalization("Localization/Resource");
 
-            services.AddRazorPages(a=>a.Conventions.AddPageRoute("/Home/Index",""));
+            services.WebEncoderConfig();
+
+            services.AddRazorPage();
+
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -35,6 +37,8 @@ namespace PrancaBeauty.WebApp
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseLocalization(new List<CultureInfo> { new CultureInfo("en-US"), new CultureInfo("fa-IR") }, "fa-IR");
 
             app.UseEndpoints(endpoints =>
             {

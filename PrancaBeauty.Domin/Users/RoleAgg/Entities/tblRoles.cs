3ec862cd990ae33a96b085f170a5f193bee38260
Reza <Reza@DESKTOP-N211H1V>
2021-01-25 23:18:49 +0330

    پیاده سازی Identity - بخش اول

---
+++ PrancaBeauty.Domin/Users/RoleAgg/Entities/tblRoles.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrancaBeauty.Domin.Users.RoleAgg.Entities
+{
+    public class tblRoles : IdentityRole<Guid>
+    {
+
+    }
+}
---
+++ PrancaBeauty.Infrastructure.EFCore/Context/MainContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PrancaBeauty.Domin.Users.RoleAgg.Entities;
+using PrancaBeauty.Domin.Users.UserAgg.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrancaBeauty.Infrastructure.EFCore.Context
+{
+    public class MainContext : IdentityDbContext<tblUsers, tblRoles, Guid>
+    {
+        public MainContext(DbContextOptions<MainContext> options) : base(options)
+        {
+
+        }
+
+        public MainContext()
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}

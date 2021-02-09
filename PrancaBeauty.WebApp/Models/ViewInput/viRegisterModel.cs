dfeb2ad3784d042580ed9db2b56d864231006bf7
Reza <Reza@DESKTOP-N211H1V>
2021-02-09 23:40:29 +0330

    بخش 38 - ثبت نام کاربران

---
+++ PrancaBeauty.WebApp/Models/ViewInput/viRegisterModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrancaBeauty.WebApp.Models.ViewInput
+{
+    public class viRegisterModel
+    {
+        [Display(Name ="Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [Display(Name = "FirstName")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "LastName")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "ConfirmPassword")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
---
+++ PrancaBeauty.WebApp/Pages/Auth/Register.cshtml.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PrancaBeauty.WebApp.Models.ViewInput;
+
+namespace PrancaBeauty.WebApp.Pages.Auth
+{
+    public class RegisterModel : PageModel
+    {
+        public RegisterModel()
+        {
+
+        }
+
+        public IActionResult OnGet()
+        {
+
+            return Page();
+        }
+
+        public viRegisterModel Input { get; set; }
+    }
+}

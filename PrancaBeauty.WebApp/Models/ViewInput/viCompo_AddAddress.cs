0a608c8dfdc5c8122adcc4474ebbc8501af68cec
Reza <Reza@DESKTOP-N211H1V>
2021-06-13 18:16:46 +0430

    جلسه 122- پروفایل کاربری - قسمت هفدهم

---
+++ PrancaBeauty.WebApp/Models/ViewInput/viCompo_AddAddress.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrancaBeauty.WebApp.Models.ViewInput
+{
+    public class viCompo_AddAddress
+    {
+        [Display(Name = "CountryId")]
+        [Required(ErrorMessage = "RequiredMsg")]
+        public string CountryId { get; set; }
+
+        [Display(Name = "ProviceId")]
+        [Required(ErrorMessage = "RequiredMsg")]
+        public string ProviceId { get; set; }
+
+        [Display(Name = "CityId")]
+        [Required(ErrorMessage = "RequiredMsg")]
+        public string CityId { get; set; }
+
+        [Display(Name = "District")]
+        [Required(ErrorMessage = "RequiredMsg")]
+        public string District { get; set; } // محله
+
+        [Display(Name = "Address")]
+        [Required(ErrorMessage = "RequiredMsg")]
+        public string Address { get; set; }
+
+        [Display(Name = "Plaque")]
+        [Required(ErrorMessage = "RequiredMsg")]
+        public string Plaque { get; set; } // پلاک
+
+        [Display(Name = "Unit")]
+        public string Unit { get; set; }
+
+        [Display(Name = "PostalCode")]
+        [Required(ErrorMessage = "RequiredMsg")]
+        public string PostalCode { get; set; }
+
+        [Display(Name = "NationalCode")]
+        [Required(ErrorMessage = "RequiredMsg")]
+        public string NationalCode { get; set; }
+
+        [Display(Name = "FirstName")]
+        [Required(ErrorMessage = "RequiredMsg")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "LastName")]
+        [Required(ErrorMessage = "RequiredMsg")]
+        public string LastName { get; set; }
+
+        [Display(Name = "PhoneNumber")]
+        [Required(ErrorMessage = "RequiredMsg")]
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ PrancaBeauty.WebApp/Pages/User/EditProfile/Components/Address/Compo_AddAddress.cshtml.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Framework.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PrancaBeauty.Application.Apps.Address;
+using PrancaBeauty.WebApp.Common.Utility.MessageBox;
+using PrancaBeauty.WebApp.Models.ViewInput;
+
+namespace PrancaBeauty.WebApp.Pages.User.EditProfile.Components.Address
+{
+    public class Compo_AddAddressModel : PageModel
+    {
+        private readonly IMsgBox _MsgBox;
+        private readonly ILocalizer _Localizer;
+        private readonly IAddressApplication _AddressApplication;
+        public Compo_AddAddressModel(ILocalizer localizer, IMsgBox msgBox)
+        {
+            _Localizer = localizer;
+            _MsgBox = msgBox;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public viCompo_AddAddress Input { get; set; }
+    }
+}

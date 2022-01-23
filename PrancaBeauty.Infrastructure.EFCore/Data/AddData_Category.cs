7f289d63d5ffaacca15de169138832dc5f588cd2
Reza <Reza@DESKTOP-N211H1V>
2022-01-23 23:00:25 +0330

    جلسه 293- رفع مشکلات سیستم - قسمت چهاردهم

---
+++ PrancaBeauty.Infrastructure.EFCore/Data/AddData_Category.cs
@@ -0,0 +1,75 @@
+﻿using Framework.Common.ExMethods;
+using Framework.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using PrancaBeauty.Domin.Categories.CategoriesAgg.Entities;
+using PrancaBeauty.Domin.FileServer.FileAgg.Entities;
+using PrancaBeauty.Domin.FileServer.FilePathAgg.Entities;
+using PrancaBeauty.Domin.FileServer.FileTypeAgg.Entities;
+using PrancaBeauty.Domin.Region.LanguagesAgg.Entities;
+using PrancaBeauty.Infrastructure.EFCore.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrancaBeauty.Infrastructure.EFCore.Data
+{
+    public class AddData_Category
+    {
+        BaseRepository<tblCategoris> _RepCategory;
+        BaseRepository<tblLanguages> _LanguageRepository;
+        BaseRepository<tblFilePaths> _FilePaths;
+        BaseRepository<tblFileTypes> _FileTypes;
+
+        MainContext _Context;
+        public AddData_Category()
+        {
+            _Context = new MainContext();
+            _RepCategory = new BaseRepository<tblCategoris>(_Context);
+            _LanguageRepository = new BaseRepository<tblLanguages>(_Context);
+            _FilePaths = new BaseRepository<tblFilePaths>(_Context);
+            _FileTypes = new BaseRepository<tblFileTypes>(_Context);
+        }
+        public void Run()
+        {
+            if (!_RepCategory.Get.Where(a => a.Name == "AllProducts").Any())
+            {
+                _RepCategory.AddAsync(new tblCategoris
+                {
+                    Id = new Guid().SequentialGuid(),
+                    Name = "AllProducts",
+                    ParentId = default,
+                    Sort = 0,
+                    tblCategory_Translates = new List<tblCategory_Translates>
+                    {
+                        new tblCategory_Translates{
+                             Id = new Guid().SequentialGuid(),
+                             LangId=_LanguageRepository.Get.Where(a=>a.Code=="fa-IR").Select(a=>a.Id).Single(),
+                             Title="همه محصولات",
+                             Description=""
+                        },
+                        new tblCategory_Translates{
+                             Id = new Guid().SequentialGuid(),
+                             LangId=_LanguageRepository.Get.Where(a=>a.Code=="en-US").Select(a=>a.Id).Single(),
+                             Title="All Products",
+                             Description=""
+                        }
+                    },
+                    tblFiles = new tblFiles()
+                    {
+                        Id = new Guid().SequentialGuid(),
+                        FilePathId = _FilePaths.Get.Where(a => a.Path == "/image/png/2021/1/1/").Where(a => a.tblFileServer.Name == "Public").Select(a => a.Id).Single(),
+                        Title = "AllProducts",
+                        Date = DateTime.Now,
+                        FileName = "AllProducts.png",
+                        FileTypeId = _FileTypes.Get.Where(a => a.MimeType == "image/png").Select(a => a.Id).Single(),
+                        SizeOnDisk = 0
+                    }
+                }, default, false).Wait();
+            }
+
+            _RepCategory.SaveChangeAsync().Wait();
+        }
+    }
+}
--- PrancaBeauty.Infrastructure.EFCore/Data/AddData_FileServers.cs
+++ PrancaBeauty.Infrastructure.EFCore/Data/AddData_FileServers.cs
@@ -1,10 +1,13 @@
-﻿using Framework.Common.ExMethods;
+﻿using Framework.Application.Consts;
+using Framework.Common.ExMethods;
 using Framework.Infrastructure;
 using PrancaBeauty.Domin.FileServer.ServerAgg.Entities;
 using PrancaBeauty.Infrastructure.EFCore.Context;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,14 +23,23 @@ namespace PrancaBeauty.Infrastructure.EFCore.Data
 
         public void Run()
         {
+
             if (!_repFileServer.Get.Any(a => a.Name == "Public"))
             {
+                #region Encrypt FtpData
+                string _FtpData = "";
+                {
+                    string JsonData = File.ReadAllText($@"{AssemblyDirectory}\Data\AddData_FileServers.json", Encoding.UTF8);
+                    _FtpData = JsonData.AesEncrypt(AuthConst.SecretKey);
+                }
+                #endregion
+
                 _repFileServer.AddAsync(new tblFileServers()
                 {
                     Id = new Guid().SequentialGuid(),
                     Name = "Public",
-                    FtpData = "0O9m6iLTbav1UXlihSeD5uq6LEZEIwDs87i4I2gMhrPCeLBEd4OH9fGJ93AbUpIgWtcpOTiGo0LVGUX9T5WZ2S/VNaQFfQjEWNORjEUMHuYk8yob5KakMFh4iwfDYCEu",
-                    Capacity = 0,
+                    FtpData = _FtpData,
+                    Capacity = 10737418240,
                     Description = "",
                     HttpDomin = "http://127.0.0.111",
                     HttpPath = "/Main",
@@ -36,6 +48,17 @@ namespace PrancaBeauty.Infrastructure.EFCore.Data
             }
 
             _repFileServer.SaveChangeAsync().Wait();
+        }
+
+        public static string AssemblyDirectory
+        {
+            get
+            {
+                string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+                UriBuilder uri = new UriBuilder(codeBase);
+                string path = Uri.UnescapeDataString(uri.Path);
+                return Path.GetDirectoryName(path);
+            }
         }
     }
 }

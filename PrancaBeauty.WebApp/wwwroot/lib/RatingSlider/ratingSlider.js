b581c9971af30da1a9ffa21c1f97cd3b8fe720ca
MohammadReza Ahmadi <dotnetlearn@hotmail.com>
2022-02-18 15:55:34 +0330

    جلسه 312- نمایش جزئیات محصول به خریدار - قسمت  یازدهم

--- PrancaBeauty.WebApp/wwwroot/lib/RatingSlider/ratingSlider.js
+++ PrancaBeauty.WebApp/wwwroot/lib/RatingSlider/ratingSlider.js
@@ -1,4 +1,4 @@
-var step = 10;
+var step = 5;
 
 function setPathData(path, value) {
     var firstStep = 6 / step * value;
@@ -6,23 +6,52 @@ function setPathData(path, value) {
     path.attr('d', 'M1,' + (7 - firstStep) + ' C6.33333333,' + (2 + secondStep) + ' 11.6666667,' + (1 + firstStep) + ' 17,' + (1 + firstStep) + ' C22.3333333,' + (1 + firstStep) + ' 27.6666667,' + (2 + secondStep) + ' 33,' + (7 - firstStep));
 }
 
-function LoadRatingSlider(_SliderId, _Step=10) {
-    step = _Step;
+function LoadRatingSlider(_SliderId, _SelectedRateId, _ElementToAddValue) {
     $('#' + _SliderId).each(function () {
         var self = $(this);
         var slider = self.slider({
             create: function () {
-                self.find('.text strong').text(self.slider('value'));
+                $('#' + _ElementToAddValue).val(self.slider('value'));
+
+                $('#' + _SelectedRateId).css('color', 'red');
+                $('#' + _SelectedRateId).text(WithoutRate);
+
                 setPathData(self.find('.smiley').find('svg path'), self.slider('value'));
             },
             slide: function (event, ui) {
-                self.find('.text strong').text(ui.value);
+                $('#' + _SelectedRateId).css('color', 'unset');
+                switch (ui.value) {
+                    case 0:
+                        $('#' + _SelectedRateId).css('color', 'red');
+                        $('#' + _SelectedRateId).text(WithoutRate);
+                        break;
+                    case 1:
+                        $('#' + _SelectedRateId).text(VeryBad);
+                        break;
+
+                    case 2:
+                        $('#' + _SelectedRateId).text(Bad);
+                        break;
+
+                    case 3:
+                        $('#' + _SelectedRateId).text(Good);
+                        break;
+
+                    case 4:
+                        $('#' + _SelectedRateId).text(VeryGood);
+                        break;
+
+                    case 5:
+                        $('#' + _SelectedRateId).text(Excellent);
+                        break;
+                }
+                $('#' + _ElementToAddValue).val(ui.value);
                 setPathData(self.find('.smiley').find('svg path'), ui.value);
             },
             range: 'min',
-            min: 1,
+            min: 0,
             max: step,
-            value: 1,
+            value: 0,
             step: 1
         });
 

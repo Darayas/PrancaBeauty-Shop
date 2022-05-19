8b6444a13c5c1f7997a45478c995c69c9a8db4cf
MohammadReza Ahmadi <dotnetlearn@hotmail.com>
2022-05-19 16:31:16 +0430

    جلسه 421- صفحه جستوجو - بخش یکم

---
+++ PrancaBeauty.Application.Contracts/PresentationDTO/ViewInput/viSearch.cs
@@ -0,0 +1,18 @@
+﻿using Framework.Common.DataAnnotations.String;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrancaBeauty.Application.Contracts.PresentationDTO.ViewInput
+{
+    public class viSearch
+    {
+        [Display(Name = "CategoryName")]
+        [RequiredString]
+        [MaxLengthString(100)]
+        public string CategoryName { get; set; }
+
+        [Display(Name = "Keyword")]
+        [RequiredString]
+        [MaxLengthString(100)]
+        public string Keyword { get; set; }
+    }
+}
--- PrancaBeauty.WebApp/Pages/Home/Search.cshtml.cs
+++ PrancaBeauty.WebApp/Pages/Home/Search.cshtml.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using PrancaBeauty.Application.Contracts.PresentationDTO.ViewInput;
+using PrancaBeauty.Application.Contracts.PresentationDTO.ViewModel;
 
 namespace PrancaBeauty.WebApp.Pages.Home
 {
     public class SearchModel : PageModel
     {
-        public void OnGet()
+        public async Task<IActionResult> OnGetAsync()
         {
+            return Page();
         }
+
+        [BindProperty(SupportsGet = true)]
+        public viSearch Input { get; set; }
+        public vmSearch Data { get; set; }
     }
 }
